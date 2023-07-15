b9ac0088eec2ec0a1c5bf8beee0b114c603905e0
Who <67064633+allendragoon@users.noreply.github.com>
2023-07-15 00:17:17 -0400

    New Patch

--- library/src/shared/class/class.lua
+++ library/src/shared/class/class.lua
@@ -5,10 +5,10 @@ class.__index = class
 
 --————————————————————————————————————————————————————————————————————--
 
-function class.new(data)
-	if (data ~= nil) and (type(data) ~= 'table') then warn("Data must be a table") end
+function class.new(data: string)
+	if (data ~= nil) and (type(data) ~= 'table') and not (data) then warn("Data must be a table") end
 	if data.tables and type(data.tables) == "table" then
-		local self = { class = data.tables }
+		local self = { class = tostring(data.tables) }
 
 		return setmetatable(self, class)
 	end
@@ -16,7 +16,7 @@ end
 
 --————————————————————————————————————————————————————————————————————--
 
-function class:print_tables(data)
+function class:print_tables(data: string)
 	if not (data) then return warn("No data found.") end
 	print(data)
 end
